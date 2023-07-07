e1c3b05f283896b2ae417194a75ecaa51193cb14
NicolasOrtiz05 <nicolasstivenortizcortes5@gmail.com>
2023-07-07 00:59:58 -0500

    Anade proyecto

---
+++ Assets/Scripts/PauseResumeGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseResumeGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
